baac09b80e9037f7825d721701e1b49bacbb1865
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-02-16 10:00:38 +0500

    Update Counter.jsx

--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import "./counter.css"
-//UseState Example
+//Useeffect Example
 const Counter = () => {
   const [numinc,setNumInc] = useState(0);
   useEffect(()=> {
